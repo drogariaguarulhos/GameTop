c60042058ecb0254eaa7fe0394b59f736f7680b9
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2021-11-13 13:00:02 -0300

    Teste

--- GameTop.App/Program.cs
+++ GameTop.App/Program.cs
@@ -5,6 +5,9 @@ var jogo01 = new GameTopAction(new Jogador("Carlos"));
 
 var jogo02 = new GameTopAction(new Jogador("Jose"));
 
+var jogo03 = new GameTopAction(new Jogador("Julio"));
+
+
 jogo01.Acao(0);
 jogo02.Acao(2);
-
+jogo03.Acao(1);
